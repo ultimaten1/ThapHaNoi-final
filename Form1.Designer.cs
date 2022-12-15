bfafd707372ce3a597b6c1d8c4f855a7c49efccb
Tran Thanh Dat <tthanhdat05102003@gmail.com>
2022-12-15 14:01:33 +0700

    Do an cuoi ki CTDL&GT nhom 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace ThapHaNoifinal
+{
+    partial class formTHNf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboSodia = new System.Windows.Forms.ComboBox();
+            this.lblCocNguon = new System.Windows.Forms.Label();
+            this.lblCocTrungGian = new System.Windows.Forms.Label();
+            this.lblCocDich = new System.Windows.Forms.Label();
+            this.lblLoigiai = new System.Windows.Forms.Label();
+            this.lblChonsodia = new System.Windows.Forms.Label();
+            this.lblSobuoc = new System.Windows.Forms.Label();
+            this.btnGiai = new System.Windows.Forms.Button();
+            this.btnGiailai = new System.Windows.Forms.Button();
+            this.txtLoigiai = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cboSodia
+            // 
+            this.cboSodia.FormattingEnabled = true;
+            this.cboSodia.Location = new System.Drawing.Point(528, 40);
+            this.cboSodia.Name = "cboSodia";
+            this.cboSodia.Size = new System.Drawing.Size(121, 24);
+            this.cboSodia.TabIndex = 1;
+            this.cboSodia.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lblCocNguon
+            // 
+            this.lblCocNguon.AutoSize = true;
+            this.lblCocNguon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCocNguon.Location = new System.Drawing.Point(225, 496);
+            this.lblCocNguon.Name = "lblCocNguon";
+            this.lblCocNguon.Size = new System.Drawing.Size(76, 18);
+            this.lblCocNguon.TabIndex = 2;
+            this.lblCocNguon.Text = "Cọc Nguồn";
+            // 
+            // lblCocTrungGian
+            // 
+            this.lblCocTrungGian.AutoSize = true;
+            this.lblCocTrungGian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCocTrungGian.Location = new System.Drawing.Point(618, 496);
+            this.lblCocTrungGian.Name = "lblCocTrungGian";
+            this.lblCocTrungGian.Size = new System.Drawing.Size(102, 18);
+            this.lblCocTrungGian.TabIndex = 2;
+            this.lblCocTrungGian.Text = "Cọc Trung Gian";
+            // 
+            // lblCocDich
+            // 
+            this.lblCocDich.AutoSize = true;
+            this.lblCocDich.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCocDich.Location = new System.Drawing.Point(1032, 496);
+            this.lblCocDich.Name = "lblCocDich";
+            this.lblCocDich.Size = new System.Drawing.Size(62, 18);
+            this.lblCocDich.TabIndex = 2;
+            this.lblCocDich.Text = "Cọc Đích";
+            // 
+            // lblLoigiai
+            // 
+            this.lblLoigiai.AutoSize = true;
+            this.lblLoigiai.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLoigiai.Location = new System.Drawing.Point(1200, 26);
+            this.lblLoigiai.Name = "lblLoigiai";
+            this.lblLoigiai.Size = new System.Drawing.Size(55, 18);
+            this.lblLoigiai.TabIndex = 2;
+            this.lblLoigiai.Text = "Lời giải:";
+            // 
+            // lblChonsodia
+            // 
+            this.lblChonsodia.AutoSize = true;
+            this.lblChonsodia.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblChonsodia.Location = new System.Drawing.Point(528, 19);
+            this.lblChonsodia.Name = "lblChonsodia";
+            this.lblChonsodia.Size = new System.Drawing.Size(82, 18);
+            this.lblChonsodia.TabIndex = 2;
+            this.lblChonsodia.Text = "Chọn số đĩa";
+            // 
+            // lblSobuoc
+            // 
+            this.lblSobuoc.AutoSize = true;
+            this.lblSobuoc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSobuoc.Location = new System.Drawing.Point(1200, 302);
+            this.lblSobuoc.Name = "lblSobuoc";
+            this.lblSobuoc.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblSobuoc.Size = new System.Drawing.Size(62, 18);
+            this.lblSobuoc.TabIndex = 2;
+            this.lblSobuoc.Text = "Số bước:";
+            // 
+            // btnGiai
+            // 
+            this.btnGiai.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGiai.Location = new System.Drawing.Point(55, 40);
+            this.btnGiai.Name = "btnGiai";
+            this.btnGiai.Size = new System.Drawing.Size(80, 30);
+            this.btnGiai.TabIndex = 3;
+            this.btnGiai.Text = "Giải";
+            this.btnGiai.UseVisualStyleBackColor = true;
+            this.btnGiai.Click += new System.EventHandler(this.btnGiai_Click);
+            // 
+            // btnGiailai
+            // 
+            this.btnGiailai.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGiailai.Location = new System.Drawing.Point(206, 40);
+            this.btnGiailai.Name = "btnGiailai";
+            this.btnGiailai.Size = new System.Drawing.Size(80, 30);
+            this.btnGiailai.TabIndex = 3;
+            this.btnGiailai.Text = "Giải lại";
+            this.btnGiailai.UseVisualStyleBackColor = true;
+            this.btnGiailai.Click += new System.EventHandler(this.btnGiailai_Click);
+            // 
+            // txtLoigiai
+            // 
+            this.txtLoigiai.Location = new System.Drawing.Point(1200, 47);
+            this.txtLoigiai.Multiline = true;
+            this.txtLoigiai.Name = "txtLoigiai";
+            this.txtLoigiai.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLoigiai.Size = new System.Drawing.Size(260, 252);
+            this.txtLoigiai.TabIndex = 4;
+            // 
+            // formTHNf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1882, 653);
+            this.Controls.Add(this.txtLoigiai);
+            this.Controls.Add(this.btnGiailai);
+            this.Controls.Add(this.btnGiai);
+            this.Controls.Add(this.lblSobuoc);
+            this.Controls.Add(this.lblLoigiai);
+            this.Controls.Add(this.lblChonsodia);
+            this.Controls.Add(this.lblCocDich);
+            this.Controls.Add(this.lblCocTrungGian);
+            this.Controls.Add(this.lblCocNguon);
+            this.Controls.Add(this.cboSodia);
+            this.Name = "formTHNf";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.formTHNf_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboSodia;
+        private System.Windows.Forms.Label lblCocNguon;
+        private System.Windows.Forms.Label lblCocTrungGian;
+        private System.Windows.Forms.Label lblCocDich;
+        private System.Windows.Forms.Label lblLoigiai;
+        private System.Windows.Forms.Label lblChonsodia;
+        private System.Windows.Forms.Label lblSobuoc;
+        private System.Windows.Forms.Button btnGiai;
+        private System.Windows.Forms.Button btnGiailai;
+        private System.Windows.Forms.TextBox txtLoigiai;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThapHaNoifinal
+{
+    public partial class formTHNf : Form
+    {
+        public formTHNf()
+        {
+            InitializeComponent();
+        }
+        bool bStop = true;
+        int sodia;
+        int sobuoc = 0;
+        Label[] Dia;
+        Label[] Kiemtra = new Label[3];
+        
+        Stack<Label> cocN = new Stack<Label>();
+        Stack<Label> cocTG = new Stack<Label>();
+        Stack<Label> cocD = new Stack<Label>();
+       
+        private void frmMain_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics Gr = e.Graphics;
+            Pen pe = new Pen(Color.Brown);
+            // Vẽ Cọc 1
+            Point p1 = new Point(200, 400);
+            Point p2 = new Point(200, 100);
+            Gr.DrawLine(pe, p1, p2);
+            p1.X = 100; p1.Y = 400;
+            p2.X = 300; p2.Y = 400;
+            Gr.DrawLine(pe, p1, p2);
+            //Vẽ Cọc 2
+            p1.X = 500; p1.Y = 400;
+            p2.X = 500; p2.Y = 100;
+            Gr.DrawLine(pe, p1, p2);
+            p1.X = 400; p1.Y = 400;
+            p2.X = 600; p2.Y = 400;
+            Gr.DrawLine(pe, p1, p2);
+            //Vẽ Cọc 3
+            p1.X = 800; p1.Y = 400;
+            p2.X = 800; p2.Y = 100;
+            Gr.DrawLine(pe, p1, p2);
+            p1.X = 700; p1.Y = 400;
+            p2.X = 900; p2.Y = 400;
+            Gr.DrawLine(pe, p1, p2);
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnGiailai.Enabled = false;
+            Random random = new Random();
+            cocN.Clear(); cocTG.Clear(); cocD.Clear();
+            this.Controls.Clear(); //xóa các đĩa cũ
+            
+            // add lại
+            this.Controls.Add(lblCocNguon);
+            this.Controls.Add(lblCocTrungGian);
+            this.Controls.Add(lblCocDich);
+            this.Controls.Add(lblChonsodia);
+            this.Controls.Add(btnGiai);
+            this.Controls.Add(cboSodia);
+            this.Controls.Add(btnGiailai);
+            this.Controls.Add(txtLoigiai);
+            this.Controls.Add(lblLoigiai);
+            this.Controls.Add(lblSobuoc);
+            lblSobuoc.Text = "Số bước: 0";
+            sodia = int.Parse(cboSodia.SelectedItem.ToString());
+           
+            // Gán thuộc tính cho label kiểm tra
+            for (int i = 0; i < 2; i++)
+            {
+                Kiemtra[i] = new Label();
+                Kiemtra[i].Height = 20; Kiemtra[i].Width= 20; Kiemtra[i].Left = 1880; Kiemtra[i].Top = 680;
+                Kiemtra[i].BackColor = Color.White;
+                Kiemtra[i].Text = i.ToString();
+            }
+            cocN.Push(Kiemtra[0]); cocTG.Push(Kiemtra[1]); cocD.Push(Kiemtra[2]);
+
+            // thêm đĩa
+            Dia = new Label[sodia];
+            for (int i = 0; i< sodia; i++)
+            {
+                Dia[i] = new Label();
+                // Gán thuộc tính cho label đĩa
+                Dia[i].Height = 20;
+                Dia[i].BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+                if (i == 0)
+                {
+                    Dia[i].Width = 100;
+                    Dia[i].Left = 150;
+                    Dia[i].Top = 400 - 20 * sodia;
+                }
+                else
+                {
+                    Dia[i].Width = Dia[i - 1].Width + 40;
+                    Dia[i].Left = Dia[i - 1].Left - 20;
+                    Dia[i].Top = Dia[i - 1].Top + 20;
+                }
+                this.Controls.Add(Dia[i]);
+            }
+            for (int i = sodia - 1; i >= 0; i--)
+                cocN.Push(Dia[i]);            
+        }
+
+        private void btnGiai_Click(object sender, EventArgs e)
+        {
+            txtLoigiai.Text = "";
+            if (btnGiai.Text == "Giải")
+            {
+                if (btnGiailai.Enabled)
+                {
+                    MessageBox.Show("Vui lòng chọn số đĩa !!", "ERROR");
+                    return;
+                }
+                btnGiailai.Enabled = true;
+                btnGiai.Text = "Dừng";
+                sobuoc = 0;
+                bStop = false;
+                ChuyenDia(sodia, cocN, cocTG, cocD);
+                LoiGiaiChuyenDia(sodia, "Cọc Nguồn", "Cọc Trung Gian", "Cọc Đích");
+                btnGiai.Text = "Giải";
+                bStop = true;
+            }
+            else
+            {
+                btnGiai.Text = "Giải";
+                bStop = true;
+            }
+        }
+        // Lời giải chuyển đĩa
+        public void LoiGiaiChuyenDia(int SoDia, string CocNguon, string CocTG, string CocDich)
+        {
+            if (SoDia == 1)
+            {
+                txtLoigiai.Text = txtLoigiai.Text + "\n" + CocNguon + " ---------> " + CocDich + "\r\n";
+            }
+            else
+            {
+                LoiGiaiChuyenDia(SoDia - 1, CocNguon, CocDich, CocTG);
+                LoiGiaiChuyenDia(1, CocNguon, CocTG, CocDich);
+                LoiGiaiChuyenDia(SoDia - 1, CocTG, CocNguon, CocDich);
+            }
+        }
+
+        private void formTHNf_Load(object sender, EventArgs e)
+        {
+            // Add item số đĩa cho combobox
+            cboSodia.Items.Clear();
+            for (int i = 1; i <= 8; i++)
+                cboSodia.Items.Add(i);
+        }
+        private void btnGiailai_Click(object sender, EventArgs e)
+        {
+            // Gọi lại events của combobox
+            comboBox1_SelectedIndexChanged(sender, e);
+        }
+        private void HieuUng(Stack<Label> cocnguon, Stack<Label> cocdich)
+        {
+            
+            lblSobuoc.Text = "Số bước: " + sobuoc.ToString();
+            Label lblDia = Dia[0];
+            int speed = 1; // Tốc độ di chuyển đĩa
+            // Tìm đĩa cần chuyển (đĩa đầu ở cocnguon)
+            for (int i = 0; i < sodia; i++)
+                if (cocnguon.Contains(Dia[i]) == true)
+                {
+                    lblDia = Dia[i];
+                    break;
+                }
+
+            // Tính số đĩa đã có ở cột đích
+            int demDich =  cocdich.Count - 1;
+
+            // Nâng đĩa lên trên
+            while (lblDia.Top > 100)
+            {
+                lblDia.Top -= speed;
+                Application.DoEvents();
+            }
+
+            // Dịch trái phải
+            int vitrimoi = cocdich.Contains(Kiemtra[0]) == true ? 200 : cocdich.Contains(Kiemtra[1]) == true ? 500 : 800;
+            if (lblDia.Left + lblDia.Width / 2 < vitrimoi)
+            {
+                while (lblDia.Left + lblDia.Width / 2 < vitrimoi)
+                {
+                    lblDia.Left += speed;
+                    Application.DoEvents();
+                }
+            }
+            else if (lblDia.Left + lblDia.Width / 2 > vitrimoi)
+            {
+                while (lblDia.Left + lblDia.Width / 2 > vitrimoi)
+                {
+                    lblDia.Left -= speed;
+                    Application.DoEvents();
+                }
+            }
+            // Đặt đĩa xuống
+            while (lblDia.Top < 380 - demDich * 20)
+            {
+                lblDia.Top += speed;
+                Application.DoEvents();
+            }
+            cocdich.Push(cocnguon.Pop());
+        }
+        // Giải thuật
+        public void ChuyenDia (int soDia, Stack<Label> cocnguon, Stack<Label> coctrunggian, Stack<Label> cocdich)
+        {
+            if (soDia > 0)
+            {                
+                ChuyenDia(soDia - 1, cocnguon, cocdich, coctrunggian);
+                
+                sobuoc++;
+                if (bStop) return; // dung lai neu stop
+                HieuUng(cocnguon, cocdich);
+                ChuyenDia(soDia - 1, coctrunggian, cocnguon, cocdich);
+                
+                if (bStop) return;
+            }
+        }
+    }
+}
